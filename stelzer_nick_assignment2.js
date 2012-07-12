baa6eae0243f2250ad0f32f30f3d998dab1bf78b
Nick Stelzer <nickstelzer@Nicks-MacBook-Pro.local>
2012-07-12 08:30:27 -0400

    Fixed Procedure, Added Boolean Function

--- stelzer_nick_assignment2.js
+++ stelzer_nick_assignment2.js
@@ -4,12 +4,39 @@
 // Typical start of a vacation day
 
 // Variables
-breakfastFood = "Frosted Flakes"
+var breakfastFood = "Frosted Flakes",
+	currentWeather = "sunny",
+	idealWeather = "sunny",
+	destinationInfo = [],
+	family = ["Papa", "Grandma", "Aunt Mary", "Aunt Sharon", "Mom", "Dad", "Nick", "Deanna", "Danielle"]
+;
+	
+
 
 // Procedure
 var haveBreakfast = function (food) {
-	console.log("I'm having " + food + " for breakfast today.");
+	if (food === "Frosted Flakes") {
+		console.log("I'm having " + food + " for breakfast today, which is my favorite cereal!");
+	} else {
+		console.log("I'm having " + food + " for breakfast today.");
+	};
 };
+
+// Boolean Function 
+var whereAreWeGoingToday = function (currentWeather, idealWeather) {
+	if (currentWeather === idealWeather) {
+		console.log("The weather looks good today, so I think we'll go to the beach.");
+		return ["beach", 10];
+	} else {
+		console.log("The weather doesn't look ideal today, maybe we'll go visit Newport instead.");
+		return ["Newport", 30];
+	};
+};
+
 
 // Main Code
 haveBreakfast(breakfastFood);
+destinationInfo = whereAreWeGoingToday(currentWeather, idealWeather);
+
+
+
